076e537a9b4a029bdf44751f2c9db89b9e381733
elderguardian <katzenmaedchen@tutanota.com>
2023-06-06 22:04:26 +0200

    append images onrender

--- ImageGallery.js
+++ ImageGallery.js
@@ -5,6 +5,31 @@ class ImageGallery {
         this.config = config
     }
 
+    #renderImageElement(parent, imageUrl) {
+
+        const imageContainer = document.createElement('div')
+        imageContainer.setAttribute('class', 'image-container')
+
+        const imageElement = document.createElement('img')
+        imageElement.src = imageUrl
+        imageElement.loading = 'lazy'
+        imageElement.alt = 'Random image'
+        imageContainer.appendChild(imageElement)
+
+        return imageContainer
+    }
+
+    #appendImages(imageContainer, amount, adapter) {
+
+        for (let i = 0; i < amount; i++) {
+            adapter().then(async newImageUrl => {
+                const imageElement = this.#renderImageElement(imageContainer, newImageUrl)
+                await imageContainer.appendChild(imageElement)
+            })
+        }
+
+    }
+
     render() {
         const galleryElement = document.querySelector(this.selector)
         const imageContainer = document.createElement('div')
@@ -14,6 +39,8 @@ class ImageGallery {
         galleryEnd.setAttribute('id', 'end')
         galleryElement.appendChild(imageContainer)
         galleryElement.appendChild(galleryEnd)
+
+        this.#appendImages(imageContainer, this.config.imagesAtOnce, this.config.adapter)
     }
 
 }
